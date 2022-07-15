eaeee9fb9b78531fab2f3017cccd6f0c9e6b93be
dejanV91 <dejanvukoicic91@gmail.com>
2022-07-15 23:47:43 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,89 +1,79 @@
-const messageDiv = document.querySelector(".message");
-const clearDiv = document.querySelector(".clearSection");
-
-const clearAllBtn = document.querySelector(".clearItems");
-const content = document.querySelector(".content");
-
-const paragraphMessage= document.getElementById("message");
-const inputSection = document.getElementById("inputField");
+//====CONST
+const form = document.querySelector(".formSection");
+const grocery = document.getElementById("inputField");
 const submitBtn = document.getElementById("btn");
-
-const list = [];
-let sumItems= "";
-
-submitBtn.addEventListener("click", (a)=>{
-    addItem(a);
-});
-
-clearAllBtn.addEventListener("click", deleteAllItems);
-
-
-
-
-
-
-function addItem (e){
-    e.preventDefault();
-    let inputSet = inputSection.value;
-
-    if (inputSet.trim() !== "") {
-    let single = 
-            `<article class="singleItem">
-            <h4>${inputSet}</h4>
-            <div class="icons">
-                <span class="material-symbols-outlined edit">
-                    border_color
-                </span>
-                <span  class="material-symbols-outlined delete">
-                    delete
-                </span>
-            </div>
-            </article>`;
-
-    
-    // insert items in list
-    list.push(single);
-
-    // add all insert(string) in variable
-    sumItems += list[list.length-1];
-    content.innerHTML = sumItems;
-
-    var singleItemDiv = document.querySelectorAll(".singleItem");
-    singleItemDiv.forEach(function(e){
-        e.addEventListener("click", function(a){
-            let deleteBtn = a.target.querySelector(".delete");
-            deleteBtn.addEventListener("click",function(){
-                console.log(a.target);
-            });
-        });
-    });
-
-    
-    paragraphMessage.innerHTML = "item added in list";
-    messageDiv.classList.add("show");
-    
-    paragraphMessage.style.backgroundColor = "rgb(204, 252, 192)"
-    setTimeout(()=>{messageDiv.classList.remove("show")
-    },2000);
-    clearDiv.classList.add("show");
+const content = document.querySelector(".content");
+const alertDiv = document.getElementById("message");
+const clearBtn = document.querySelector(".clearSection");
+//==== EVENT LISTENERS
+form.addEventListener("submit",addItem);
+//edit option
+editItem = "";
+editStatus = false;
+//==== FUNCTIONS
+// add item
+function addItem(a){
+    a.preventDefault();
+    const element = grocery.value;
+    if(element && !editStatus){
+       const item = document.createElement("article");
+       const attr = document.createAttribute("class");
+       attr.value = "singleItem";
+       item.setAttributeNode(attr);
+       item.innerHTML = `<h4>${element}</h4>
+                <div class="icons">
+                    <span id="edit" class="material-symbols-outlined">
+                        border_color
+                    </span>
+                    <span id="delete" class="material-symbols-outlined">
+                        delete
+                    </span>
+                </div>`;
+        //append child
+        content.appendChild(item);
+        //alert message
+        alertDisplay("added item", "greenAlert");
+        setBack();
+        showClear();
+        clearBtn.addEventListener("click", clearAll);
+    }    
+}
+//alert message
+function alertDisplay(alert, action){
+    alertDiv.textContent = alert;
+    alertDiv.classList.add(action);
+    setTimeout(function(){
+        alertDiv.textContent="";
+        alertDiv.classList.remove(action);
+    },1000);
+}
+//set back
+function setBack(){
+    editItem = "";
+    editStatus = false;
+    grocery.value = "";
+}
+//clear all items
+function showClear(){
+    if (content.children.length > 0) {
+        clearBtn.classList.add("show");
+        // clearBtn.addEventListener("click", function(){
+        //     const element = document.querySelectorAll("singleItem");
+        //     element.forEach(function(item){
+        //         content.removeChild(item);
+        //     });
+        // });
+    }
+    else{
+        clearBtn.classList.remove("show");
     }
 }
-
-function deleteAllItems(){
-    while (list.length > 0) {
-        list.pop();
-    }
-    sumItems="";
-    content.innerHTML = "" ;
-
-    paragraphMessage.innerHTML = "cleared all items";
-    messageDiv.classList.add("show");
-    
-    paragraphMessage.style.backgroundColor = "red"
-    setTimeout(()=>{messageDiv.classList.remove("show")
-    },3000);
-    clearDiv.classList.remove("show");
-    }
-
-
-
+//clear all
+function clearAll(){
+   const element = document.querySelectorAll(".singleItem");
+        element.forEach(function(item){
+        content.removeChild(item);
+    });
+    alertDisplay("remove all items", "redAlert");
+    showClear();
+};
